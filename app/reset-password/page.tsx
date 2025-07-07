37f298ee7fe59015dc9a01db8e76e205fefd3b33
Namrebos <dienaszaglis@gmail.com>
2025-07-07 23:44:44 +0300

    space shit fix

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -3,6 +3,7 @@
 import { Suspense } from 'react'
 import ActualResetPasswordForm from './ResetPasswordForm'
 
+
 export default function ResetPasswordPage() {
   return (
     <Suspense fallback={<div className="p-4 text-center">Notiek ielāde...</div>}>
